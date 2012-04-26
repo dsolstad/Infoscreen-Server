e2643385c59b83f9fa6dfac7551be83755cab32b
echofish <echofish@echofish.org>
2012-04-26 16:56:33 +0200

    small correction

--- php/get_data.php
+++ php/get_data.php
@@ -4,17 +4,16 @@ header('Content-type: application/json');
 include("./systemstats.class.php");
 
 $stats = array();
-$q = $_GET['q'];
 
-if ($q == "all") {
+if ($_GET['q'] == 'all') {
     $class_methods = get_class_methods('SystemStats');
     foreach ($class_methods as $method_name) {
         $stats[$method_name] = SystemStats::$method_name();
     }
-} else if ($q == "cpu") {
+} else if ($_GET['q'] == 'cpu') {
     $stats['cpu_load_perc_free'] = SystemStats::cpu_load_perc_free();
     $stats['cpu_load_perc_used'] = SystemStats::cpu_load_perc_used();
-} else if ($q == "network") {
+} else if ($_GET['q'] == 'network') {
     $stats['dl_speed'] = SystemStats::dl_speed();
     $stats['ul_speed'] = SystemStats::ul_speed();
 }
